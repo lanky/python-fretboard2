b4e96c717f4d79c95ceefb64fbc687b87fde7dcd
Stuart Sears <stuart@sjsears.com>
2023-03-02 08:07:39 +0000

    update setup.py to v1.1.0

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 
 try:
-    from setuptools import setup, find_packages
+    from setuptools import find_packages, setup
 except ImportError:
-    from distutils.core import setup, find_packages
+    from distutils.core import find_packages, setup
 
-with open('./requirements.txt') as fd:
+with open("./requirements.txt") as fd:
     requirements = fd.read().splitlines()
 
-__version__ = '1.0.0'
+__version__ = "1.1.0"
 
 setup(
-    name='fretboard',
+    name="fretboard",
     version=__version__,
-    license='MIT',
-
-    author='Derek Payton',
-    author_email='derek.payton@gmail.com',
-
-    description='Fretboard is a python library for generating SVG fretboard images and chord charts in Python.',
-    long_description=open('README.rst').read(),
-
-    url='https://github.com/dmpayton/python-fretboard',
-    download_url='https://github.com/dmpayton/python-fretboard/tarball/v%s' % __version__,
-
+    license="MIT",
+    author="Derek Payton",
+    author_email="derek.payton@gmail.com",
+    description="Fretboard is a python library for generating SVG fretboard images and chord charts in Python.",
+    long_description=open("README.rst").read(),
+    url="https://github.com/dmpayton/python-fretboard",
+    download_url="https://github.com/dmpayton/python-fretboard/tarball/v%s"
+    % __version__,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -32,8 +29,7 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
-    keywords='guitar fretboard chord',
-
-    packages=['fretboard'],
+    keywords="guitar fretboard chord",
+    packages=["fretboard"],
     install_requires=requirements,
 )
