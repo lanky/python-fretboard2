8817f8cba43a5406bb5cd060665985c198183410
Stuart Sears <stuart@sjsears.com>
2023-03-09 08:48:15 +0000

    Add top-level imports for all the new classes

--- fretboard2/__init__.py
+++ fretboard2/__init__.py
@@ -5,8 +5,8 @@ import collections.abc
 for type_name in collections.abc.__all__:
     setattr(collections, type_name, getattr(collections.abc, type_name))
 
-from .chord import BassChord, Chord, UkuleleChord  # noqa: E402
-from .fretboard import Fretboard  # noqa: E402
+from .chord import BassChord, GuitarChord, UkuleleChord  # noqa: E402
+from .fretboard import BassFretboard, GuitarFretboard, UkuleleFretboard  # noqa: E402
 
 __version__ = "1.0.0"
 __author__ = "Derek Payton <derek.payton@gmail.com>"
